aff3afc0f6539d7da7e01032d355653a75483604
Gopgap <Noppawat3984@gmail.com>
2025-02-16 21:33:47 +0700

    added lazy import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import {
   ContentSkill as Skills,
   ContentWork as Works,
   ContentProject as Project,
-  ContentWrapper as Wrapper,
 } from "@/components";
-import { Fragment, Suspense } from "react";
+import { Suspense, lazy } from "react";
+
+const Wrapper = lazy(() => import("@/components/content-wrapper"));
 
 export default function MainPage() {
   return (
-    <Suspense fallback={<Fragment />}>
+    <Suspense>
       <div className="min-h-dvh py-10 px-16 max-lg:py-8 max-lg:px-12 max-sm:py-5 max-sm:px-4">
         <Intro />
         <div aria-label="wrap-contents" className="flex flex-col space-y-2">
